5bdc140bded6833a339fdae2f0aa00acd7b3eb5d
Vida May Kotico <vmkotico@yahoo.com>
2021-11-04 15:28:24 -0700

    commit tree a0695dc1bd9cd1ab002671f246ff7379bc5743ef

--- methods.rb
+++ methods.rb
@@ -1,1 +1,17 @@
-# Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+   "Hello, programmer!"
+end
+
+def greet (name = "Naureen")
+    puts "Hello, #{name}!"
+
+end
+
+def greet_with_default (name)
+  if name == String
+    puts "Hello, #{name}!"
+  else 
+    puts "Hello, programmer!"
+  end   
+end
